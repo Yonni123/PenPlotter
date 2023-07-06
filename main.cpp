3e2a5842ddde26cd1407e22e2d7242188c6a790a
Yonan <yonan.adnan707@gmail.com>
2023-07-06 23:05:57 +0200

    bitmap to mat works

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <bitset>
 
 using namespace std;
+using namespace cv;
 
-potrace_bitmap_t *potrace_bitmap_from_mat(cv::Mat image)
+potrace_bitmap_t *potrace_bitmap_from_mat(Mat image)
 {
     potrace_bitmap_t *bitmap = (potrace_bitmap_t *)malloc(sizeof(potrace_bitmap_t));
     bitmap->w = image.cols;           // Width of the bitmap
@@ -24,10 +25,10 @@ potrace_bitmap_t *potrace_bitmap_from_mat(cv::Mat image)
     bitmap->map = (potrace_word *)malloc(bitmap->dy * bitmap->h * sizeof(potrace_word));
 
     // Invert the image since potrace uses white for background (0) and black for foreground (1)
-    cv::bitwise_not(image, image);
+    bitwise_not(image, image);
 
-    cv::imshow("image", image);
-    cv::waitKey(0);
+    imshow("image", image);
+    waitKey(0);
 
     // Loop through all pixels and create potrace_words
     int current_word = 0;
@@ -65,75 +66,72 @@ potrace_bitmap_t *potrace_bitmap_from_mat(cv::Mat image)
             }
         }
     }
+
+    // Restore the bitmap's width
+    //bitmap->w = image.cols;
     return bitmap;
 }
 
-// This function saves a bitmap as a .bmp file
-void save_bitmap(potrace_bitmap_t *bitmap)
-{
-    // Create a new image
-    cv::Mat image(bitmap->h, bitmap->w, CV_8UC1);
+Mat mat_from_potrace_bitmap(potrace_bitmap_t* bitmap) {
+    int N = sizeof(potrace_word) * 8; // Number of bits per word, every bit is a pixel
 
-    // Copy the bitmap data to the image
-    int N = sizeof(potrace_word) * 8;
-    int dy = bitmap->dy;
-    potrace_word *map = bitmap->map;
+    // Create a black image with the size of the Potrace bitmap
+    Mat image(bitmap->h, bitmap->w, CV_8UC1, Scalar(0));
 
-    int ii = 2;
-    int jj = 8;
-    int pix = (map+(jj * dy))[ii / N] & (1ULL << (N - 1 - ii % N)) ? 255 : 0;
-    cout << pix << endl;
-    return; 
-    int pixel = ((map + (jj * dy))[ii / N]) & (1ULL << (N - 1 - ii % N)) ? 255 : 0;
-    cout << pixel << " ";
+    // Loop through all pixels in the Potrace bitmap
+    int current_word = 0;
+    int current_bit = 0;
+    for (int i = bitmap->h - 1; i >= 0; i--) { // Bottom to top
+        for (int j = 0; j < bitmap->w; j++) { // Left to right
+            // Get the bit value from the current potrace_word
+            potrace_word mask = (1ULL << (N - 1 - current_bit));
+            int pixel = (bitmap->map[current_word] & mask) ? 255 : 0;
 
-    return;
+            // Set the pixel value in the OpenCV Mat
+            if (j < image.cols) { // If still within the image bounds
+                image.at<uchar>(i, j) = pixel;
+            }
 
-    for (int i = 0; i < bitmap->h; i++)
-    {
-        for (int j = 0; j < bitmap->w; j++)
-        {
-            // int J = bitmap->w - 1 - j;  // OpenCV origo is top left, potrace origo is bottom left, use J for openCV
-            int pixel = ((map + j * dy)[i / N]) & (1ULL << (N - 1 - i % N)) ? 255 : 0;
-            image.at<uchar>(i, j) = pixel;
+            // Increment the current bit
+            current_bit++;
+            if (current_bit == N) {
+                current_bit = 0;
+                current_word++;
+            }
         }
-        cout << endl;
     }
 
-    // Since white pixels are foreground in potrace, invert the image
-    cv::bitwise_not(image, image);
-    cv::imwrite("output.bmp", image);
+    // Invert the image back to its original state
+    bitwise_not(image, image);
+
+    return image;
 }
+
 
 int main()
 {
     // Load and display an image using OpenCV
-    cv::Mat image = cv::imread("../TestImages/cropped.jpg", cv::IMREAD_COLOR);
+    Mat image = imread("../TestImages/edges.png", IMREAD_COLOR);
     if (image.empty())
     {
         std::cout << "Failed to load image." << std::endl;
         return 1;
     }
 
-    cv::imshow("Original Image", image);
-    cv::waitKey(0);
-
     // Convert image to grayscale
-    cv::Mat grayImage;
-    cv::cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
+    Mat grayImage;
+    cvtColor(image, grayImage, COLOR_BGR2GRAY);
 
     // Perform thresholding
-    cv::Mat thresholdedImage;
-    cv::threshold(grayImage, thresholdedImage, 128, 255, cv::THRESH_BINARY);
-
-    // Show the image
-    cv::imshow("Thresh", thresholdedImage);
-    cv::waitKey(0);
+    Mat thresholdedImage;
+    threshold(grayImage, thresholdedImage, 128, 255, THRESH_BINARY);
 
     // Convert the image to a bitmap
     potrace_bitmap_t *bitmap = potrace_bitmap_from_mat(thresholdedImage);
 
-    save_bitmap(bitmap);
+    // Display the bitmap
+    imshow("Potrace", mat_from_potrace_bitmap(bitmap));
+    waitKey(0);
 
     // Create a new potrace_state_t
     potrace_param_t *param = potrace_param_default();
