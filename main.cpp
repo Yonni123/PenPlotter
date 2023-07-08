65089110e82f4589b789419768d7925e2d83f489
Yonan <yonan.adnan707@gmail.com>
2023-07-08 21:11:32 +0200

    Checkpoint

--- main.cpp
+++ main.cpp
@@ -6,106 +6,82 @@
 using namespace std;
 using namespace cv;
 
-potrace_bitmap_t *potrace_bitmap_from_mat(Mat image)
+potrace_bitmap_t potrace_bitmap_from_mat(Mat image)
 {
-    potrace_bitmap_t *bitmap = (potrace_bitmap_t *)malloc(sizeof(potrace_bitmap_t));
-    bitmap->w = image.cols;           // Width of the bitmap
-    bitmap->h = image.rows;           // Height of the bitmap
-    int N = sizeof(potrace_word) * 8; // Number of bits per word, every bit is a pixel
+    // Create an instance of the potrace_bitmap_t struct
+    potrace_bitmap_t bitmap;
+    bitmap.w = image.cols;           // Width of the bitmap
+    bitmap.h = image.rows;           // Height of the bitmap
 
-    // Make the width a multiple of N by padding
-    if (bitmap->w % N != 0)
-    {
-        bitmap->w = (bitmap->w / N + 1) * N;
-    }
+    int N = sizeof(potrace_word) * 8;       // Number of bits per word, every bit is a pixel
+    int wWithPad = (bitmap.w / N + 1) * N;  // Width of the bitmap with padding
 
-    bitmap->dy = bitmap->w / N; // Number of words per scanline
+    bitmap.dy = wWithPad / N;               // Number of words per scanline
 
     // Allocate memory for the bitmap, we need dy*h potrace_words
-    bitmap->map = (potrace_word *)malloc(bitmap->dy * bitmap->h * sizeof(potrace_word));
+    bitmap.map = new potrace_word[bitmap.dy * bitmap.h];
+    cout << bitmap.dy * bitmap.h << endl;
 
     // Invert the image since potrace uses white for background (0) and black for foreground (1)
     bitwise_not(image, image);
 
-    imshow("image", image);
-    waitKey(0);
+    cout << "going int othe loop" << endl;
 
-    // Loop through all pixels and create potrace_words
-    int current_word = 0;
-    int current_bit = 0;
-    bitmap->map[current_word] = 0;
-    for (int i = image.rows - 1; i >= 0; i--)
-    { // Bottom to top
-        for (int j = 0; j < bitmap->w; j++)
-        { // Left to right
-            // Get the pixel value (0 or 1)
-            int pixel = 0;
-            if (j < image.cols)
-            {                                        // If still within image
-                pixel = image.at<uchar>(i, j) / 255; // Convert pixel to 0 or 1
-            }
+    int currentWord = 0;    // Current word index in the map array
+    int currentBit = 0;     // Current bit index in the current word
+    bitmap.map[currentWord] = 0;    // Initialize the first word to 0
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            uchar pixel = image.at<uchar>(i, j)/255;
 
-            // Set the bit in the potrace_word if the pixel is set
-            potrace_word mask = (1ULL << (N - 1 - current_bit));
+            // Set or reset the bit in the current word depending on the pixel value
+            potrace_word mask = 1ULL << (N - 1 - currentBit);   // Create a mask with a 1 at the current bit position
             if (pixel == 1)
-            {
-                bitmap->map[current_word] |= mask;
-            }
-            else
-            {
-                bitmap->map[current_word] &= ~mask;
-            }
+                bitmap.map[currentWord] |= mask;    // Set the bit
+            else bitmap.map[currentWord] &= ~mask;   // Reset the bit
 
-            // Increment the current bit
-            current_bit++;
-            if (current_bit == N)
-            {
-                current_bit = 0;
-                current_word++;
-                bitmap->map[current_word] = 0;
+            currentBit++;   // Move to the next bit
+            if (currentBit == N) {  // If we have filled the current word
+                currentBit = 0;     // Reset the bit index
+                currentWord++;      // Move to the next word
+                bitmap.map[currentWord] = 0;    // Initialize the next word to 0
             }
         }
+        // Since every scanLine changes the word, we need to move to the next word
+        // Since we sat the next word to 0, we don't need to do anything else
+        // The bits that we didn't visit are already 0
+        currentWord++;  // Move to the next word
+        currentBit = 0;
+        if(i == 1) break;   // Only do the first few row for debugging
     }
 
-    // Restore the bitmap's width
-    //bitmap->w = image.cols;
     return bitmap;
 }
 
 Mat mat_from_potrace_bitmap(potrace_bitmap_t* bitmap) {
     int N = sizeof(potrace_word) * 8; // Number of bits per word, every bit is a pixel
 
-    // Create a black image with the size of the Potrace bitmap
-    Mat image(bitmap->h, bitmap->w, CV_8UC1, Scalar(0));
+    // Create an empty Mat object with the desired size and type
+    //Mat image(bitmap->h, bitmap->w, CV_8UC1, Scalar(0));
 
     // Loop through all pixels in the Potrace bitmap
-    int current_word = 0;
-    int current_bit = 0;
-    for (int i = bitmap->h - 1; i >= 0; i--) { // Bottom to top
-        for (int j = 0; j < bitmap->w; j++) { // Left to right
-            // Get the bit value from the current potrace_word
-            potrace_word mask = (1ULL << (N - 1 - current_bit));
-            int pixel = (bitmap->map[current_word] & mask) ? 255 : 0;
+    for (int y = 0; y < bitmap->h; y++) { // Top to bottom
+        for (int x = 0; x < bitmap->w; x++) { // Left to right
+            // Get the pixel value from the Potrace bitmap using the formula
+            int pixel = ((bitmap->map + (y * bitmap->dy))[x / N] & (1ULL << (N - 1 - x % N))) ? 0 : 255;
 
             // Set the pixel value in the OpenCV Mat
-            if (j < image.cols) { // If still within the image bounds
-                image.at<uchar>(i, j) = pixel;
-            }
-
-            // Increment the current bit
-            current_bit++;
-            if (current_bit == N) {
-                current_bit = 0;
-                current_word++;
-            }
+            //image.at<uchar>(bitmap->h - 1 - y, x) = pixel;
+            cout << pixel/255 << " ";
         }
+        cout << endl;
     }
 
-    // Invert the image back to its original state
-    bitwise_not(image, image);
-
-    return image;
+    //return image;
+    return Mat();
 }
+
+
 
 
 int main()
@@ -117,35 +93,33 @@ int main()
         std::cout << "Failed to load image." << std::endl;
         return 1;
     }
+    imshow("image", image);
+    waitKey(0);
 
     // Convert image to grayscale
     Mat grayImage;
     cvtColor(image, grayImage, COLOR_BGR2GRAY);
+    imshow("grayImage", grayImage);
+    waitKey(0);
 
     // Perform thresholding
     Mat thresholdedImage;
     threshold(grayImage, thresholdedImage, 128, 255, THRESH_BINARY);
-
-    // Convert the image to a bitmap
-    potrace_bitmap_t *bitmap = potrace_bitmap_from_mat(thresholdedImage);
-
-    // Display the bitmap
-    imshow("Potrace", mat_from_potrace_bitmap(bitmap));
+    imshow("thresholdedImage", thresholdedImage);
     waitKey(0);
 
-    // Create a new potrace_state_t
-    potrace_param_t *param = potrace_param_default();
-    param->turdsize = 2;
-    param->turnpolicy = POTRACE_TURNPOLICY_BLACK;
-    param->alphamax = 0.1;
-    param->opticurve = 1;
-    param->opttolerance = 0.2;
-    param->progress.callback = NULL;
-    param->progress.data = NULL;
-    param->progress.min = 0.0;
-    param->progress.max = 1.0;
-    param->progress.epsilon = 0.0;
-    potrace_state_t *state = potrace_trace(param, bitmap);
+    // Convert the image to a bitmap
+    potrace_bitmap_t bitmap = potrace_bitmap_from_mat(thresholdedImage);
+    cout << "bitmap created" << endl;
+
+    // Display the bitmap
+    //Mat out = mat_from_potrace_bitmap(bitmap);
+    //imshow("bitmap", out);
+    //waitKey(0);
+
+    Mat clone = image.clone();
+    imshow("clone", clone);
+    waitKey(0);
 
     return 0;
 }
