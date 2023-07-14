1def4799723f47edd86bad2970c3c08693916293
Yonni123 <86354472+Yonni123@users.noreply.github.com>
2023-07-15 00:38:28 +0200

    Turn edge detector into functions

--- Python/EdgeDetection.py
+++ Python/EdgeDetection.py
@@ -1,8 +1,6 @@
 import cv2
 import numpy as np
 
-input_image = "../TestImages/mikasaedge.jpg"
-
 # Callback function for trackbar
 def update_threshold(val):
     threshold1 = cv2.getTrackbarPos('Threshold', 'Canny Edge Detection')
@@ -19,33 +17,64 @@ def update_threshold(val):
 
     cv2.imshow('Canny Edge Detection', edges)
 
-# Read the image
-image = cv2.imread(input_image, cv2.IMREAD_GRAYSCALE)
+def edge_detect_live(image):
+    if len(image.shape) == 3:   # If the image is not grayscale, convert it to grayscale
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Create a window
-cv2.namedWindow('Canny Edge Detection', cv2.WINDOW_NORMAL)
+    # Create a window
+    cv2.namedWindow('Canny Edge Detection', cv2.WINDOW_NORMAL)
 
-# Resize the window to fit the screen
-height = 800
-width = int(image.shape[1]/image.shape[0]*height)
-cv2.resizeWindow('Canny Edge Detection', width, height)
+    # Resize the window to fit the screen
+    height = 800
+    width = int(image.shape[1] / image.shape[0] * height)
+    cv2.resizeWindow('Canny Edge Detection', width, height)
 
-# Create a trackbar for threshold value and edge thickness
-cv2.createTrackbar('Threshold', 'Canny Edge Detection', 0, 255, update_threshold)
-cv2.createTrackbar('Thickness', 'Canny Edge Detection', 0, 10, update_threshold)
+    # Create a trackbar for threshold value and edge thickness
+    cv2.createTrackbar('Threshold', 'Canny Edge Detection', 0, 255, update_threshold)
+    cv2.createTrackbar('Thickness', 'Canny Edge Detection', 0, 10, update_threshold)
 
-# Initialize the threshold value
-threshold1 = cv2.getTrackbarPos('Threshold', 'Canny Edge Detection')
-threshold2 = 2 * threshold1
+    # Perform initial edge detection
+    threshold1 = cv2.getTrackbarPos('Threshold', 'Canny Edge Detection')
+    threshold2 = 2 * threshold1
+    edges = cv2.Canny(image, threshold1, threshold2)
+    edges = cv2.bitwise_not(edges)
+    cv2.imshow('Canny Edge Detection', edges)
 
-# Perform initial edge detection
-edges = cv2.Canny(image, threshold1, threshold2)
-edges = cv2.bitwise_not(edges)
-cv2.imshow('Canny Edge Detection', edges)
+    # Wait until the user presses 'Esc' key
+    while True:
+        key = cv2.waitKey(1)
+        try:
+            threshold1 = cv2.getTrackbarPos('Threshold', 'Canny Edge Detection')
+            thickness = cv2.getTrackbarPos('Thickness', 'Canny Edge Detection')
+        except:
+            break
+        if key == 27:  # 'Esc' key pressed
+            break
 
-# Wait until the user presses 'Esc' key
-while cv2.waitKey(0) != 27:
-    pass
+    threshold2 = 2 * threshold1
 
-# Close all windows
-cv2.destroyAllWindows()
+    # Perform edge detection with the updated threshold values
+    edges = cv2.Canny(image, threshold1, threshold2)
+
+    # Make edges thicker
+    kernel = np.ones((2, 2), np.uint8)
+    edges = cv2.dilate(edges, kernel, iterations=thickness)
+
+    # Invert the image so that the edges are black
+    edges = cv2.bitwise_not(edges)
+
+    # Close all windows
+    cv2.destroyAllWindows()
+
+    return edges
+
+
+if __name__ == "__main__":
+    input_image = "../TestImages/nami.jpg"
+    image = cv2.imread(input_image)
+    edges = edge_detect_live(image)
+
+    cv2.imshow('Original', image)
+    cv2.imshow('Canny Edge Detection', edges)
+    cv2.waitKey(0)
+
